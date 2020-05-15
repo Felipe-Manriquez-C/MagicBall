6564e61359b8f98962c8fa50dba4d66b09db8b06
felipe manriquez <felipe.manriquez1@mail.udp.cl>
2020-05-14 21:20:04 -0400

    Desarrollo y arreglo de App

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView respuesta;
 
     private String[] respuestaArray ={"Solo no puedes, con amigos sí", "Dile al coco que piense un poco", "Puede confiar en él", "El error no importa, siempre y cuando sepas corregirlo",
-            "Cuanto más grande la dificultad, mayor la gloria", "Perspectiva buena", "Las señales apuntan a sí", "No son tus años de vida lo que cuenta, sino la vida en tus años",
-            "Mejor no decirte ahora", "No puedo predecir ahora", "Levántate, suspira, sonríe, y sigue adelante", "¡Nunca te acostarás sin saber una cosa más!", "Si te caes diez veces levántate once",
+            "Cuanto más grande la dificultad, mayor la gloria", "No son tus años de vida lo que cuenta, sino la vida en tus años", "Tener éxito no es aleatorio, es una variable dependiente del esfuerzo.",
+            "Levántate, suspira, sonríe, y sigue adelante", "¡Nunca te acostarás sin saber una cosa más!", "Si te caes diez veces levántate once", "Si quieres darlo todo en esta vida, piensa en positivo.",
             "Todo lo que puedas imaginar es real", "Deja que el mundo te sorprenda", "Eres suficiente tal y como eres", " Las cosas más simples pueden traer la mayor felicidad",
             "Si el plan no funciona, cambia el plan, pero no cambies la meta", "No sueñes tu vida, vive tu sueño", "Tienes que esperar cosas de ti mismo antes de poder hacerlas",
             "La preocupación nos debería llevar a la acción y no a la depresión", "Sólo una cosa vuelve un sueño imposible: el miedo a fracasar"};
